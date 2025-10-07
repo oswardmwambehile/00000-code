974f58ab238e4768b563255cbac8a0b855d187c1
oswardmwambehile <oswardmwambehile@gmail.com>
2025-10-07 15:43:19 +0300

    customer Crud operations

--- customers/urls.py
+++ customers/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('customers/', views.customer_list, name='customer-list'),             # GET all customers
+    path('customers/create/', views.customer_create, name='customer-create'),  # POST create customer
+    path('customers/<int:pk>/', views.customer_detail, name='customer-detail'),# GET single customer
+    path('customers/<int:pk>/update/', views.customer_update, name='customer-update'), # PUT update
+    path('customers/<int:pk>/delete/', views.customer_delete, name='customer-delete'), # DELETE
    
 ]
 
