8427f1871de5edd6d36557c323c3488eed7a43ac
oswardmwambehile <oswardmwambehile@gmail.com>
2025-10-07 16:18:24 +0300

    Product crud operations implimented

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('products/', views.product_list, name='product-list'),
+    path('products/create/', views.product_create, name='product-create'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('products/<int:pk>/update/', views.product_update, name='product-update'),
+    path('products/<int:pk>/delete/', views.product_delete, name='product-delete'),
 ]
