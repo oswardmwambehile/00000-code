a43e459b685dbeba1247864b6d90d9272842c4b9
oswardmwambehile <oswardmwambehile@gmail.com>
2025-10-23 11:29:49 +0300

    user and supervisor chat message implimented for the specific visit sent to be verified by supervisor

--- verification/urls.py
+++ verification/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SubmitVisitVerificationView, UserSubmittedVerificationsView,SupervisorVisitVerificationsView, SupervisorVisitVerificationDetailView, SupervisorUpdateVerificationView
+from .views import SubmitVisitVerificationView, UserSubmittedVerificationsView,SupervisorVisitVerificationsView, SupervisorVisitVerificationDetailView, SupervisorUpdateVerificationView, UserVerificationDetailView, VerificationMessageListView, VerificationMessageCreateView, UserVerificationMessageListView, UserVerificationMessageCreateView
 
 urlpatterns = [
     path('visits/submit-verification/', SubmitVisitVerificationView.as_view(), name='submit-visit-verification'),
@@ -11,4 +11,36 @@ urlpatterns = [
         SupervisorUpdateVerificationView.as_view(),
         name='supervisor-update-verification'
     ),
+
+  path('visits/my-submissions/<int:verification_id>/',
+    UserVerificationDetailView.as_view(),
+    name='user-verification-detail'
+  ),
+
+   path(
+        'verifications/<int:verification_id>/messages/',
+        VerificationMessageListView.as_view(),
+        name='verification-messages'
+    ),
+    path(
+        'verifications/<int:verification_id>/messages/send/',
+        VerificationMessageCreateView.as_view(),
+        name='verification-send-message'
+    ),
+
+
+
+    path(
+        'user-verifications/<int:verification_id>/messages/',
+        UserVerificationMessageListView.as_view(),
+        name='verification-messages'
+    ),
+    path(
+        'user-verifications/<int:verification_id>/messages/send/',
+        UserVerificationMessageCreateView.as_view(),
+        name='verification-send-message'
+    ),
 ]
+
+
+
